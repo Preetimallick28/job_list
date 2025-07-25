16f3e62bbed89ddb158799ce1c0e1b03137e4c2f
krish1434 <krrishna1434@gmail.com>
2025-07-26 00:26:54 +0530

    Change reg patern

--- app.py
+++ app.py
@@ -4,13 +4,62 @@ import re
 
 # List of websites to check
 websites = [
-    "https://example.com",
-    "https://another-example.com",
-    # Add more websites here
+    "http://askmeguru.com/",
+    "https://www.jnettechnologies.com/",
+    "https://www.sparity.com/",
+    "https://conquerorstech.net/",
+    "http://www.brainovision.in/",
+    "https://axassoftware.com/",
+    "http://www.magneqservices.in/",
+    "https://raybiztech.com/",
+    "https://www.encora.com/",
+    "https://www.foraysoft.com/",
+    "https://www.avineon.com/en/apac/home",
+    "http://oracle.com/",
+    "http://www.eurekaitsol.com/",
+    "http://tstecnoservice.com/",
+    "http://www.experienceflow.ai/",
+    "https://greemus.com/",
+    "https://www.sisfirst.in/",
+    "http://www.pronteff.com/",
+    "https://www.savantis.com/",
+    "http://www.triloksoft.com/",
+    "http://www.metanoiasolutions.net/",
+    "http://www.shrasits.com/",
+    "https://techclouderp.com/",
+    "http://hgtechinc.net/",
+    "https://danyonittech.in/",
+    "https://www.elitecrest.in/",
+    "https://www.creativetechmars.com/",
+    "https://zenq.com/",
+    "https://www.suchirsoftech.com/",
+    "https://techweblabs.com/",
+    "http://www.tecnics.com/",
+    "https://elabsinfotech.com/",
+    "https://tekbon.com/",
+    "https://www.webappclouds.com/",
+    "http://www.zazz.io/",
+    "http://scienstechnologies.com/",
+    "https://www.iblesoft.com/",
+    "https://www.mwebware.com/",
+    "http://vagarioussolutions.com/",
+    "https://fission.it/",
+    "http://www.c2n.in/",
+    "http://www.megasoft.com/",
+    "http://www.acuvate.com/",
+    "http://vortextechnologies.net/",
+    "https://camelq.in/",
+    "https://www.kexlin.com/",
+    "https://www.techbulls.co.in/",
+    "http://www.sanoits.com/",
+    "http://votigo.com/",
+    "https://www.snovasys.com/",
+    "https://www.pramati.com/"
 ]
 
+
 # Regex pattern to match LinkedIn URLs
-linkedin_pattern = re.compile(r'https?://(www\.)?linkedin\.com/in/[^\s"\'<>]+')
+linkedin_pattern = re.compile(r'https?://([a-z]+\.)?linkedin\.com/(in|company)/[^\s"\'<>]+')
 
 # Output file
 output_file = "linkedin_links.txt"
